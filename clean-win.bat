1ff2b243a7e594b559a0fb5012d1bb1653962b3c
HerrNamenlos123 <herrnamenlos123@gmail.com>
2022-02-18 21:13:38 +0100

    Update clean-win.bat

--- clean-win.bat
+++ clean-win.bat
@@ -4,16 +4,20 @@ echo Cleaning Visual Studio solution...
 2>NUL del "%~dp0*.sln"
 
 echo Cleaning Visual Studio cache...
-2>NUL rmdir /s /q "%~dp0/.vs"
+2>NUL rmdir /s /q "%~dp0.vs"
 
 echo Cleaning bin directory...
-2>NUL rmdir /s /q "%~dp0/bin"
+2>NUL rmdir /s /q "%~dp0bin"
 
 echo Cleaning build directory...
-2>NUL rmdir /s /q "%~dp0/build"
+2>NUL rmdir /s /q "%~dp0build"
 
 echo Cleaning BatteryEngine...
 call "%~dp0modules/BatteryEngine/clean-win.bat"
+
+echo Cleaning libusbcpp...
+2>NUL rmdir /s /q "%~dp0modules/libusbcpp/bin"
+2>NUL rmdir /s /q "%~dp0modules/libusbcpp/build"
 
 echo Done
 
